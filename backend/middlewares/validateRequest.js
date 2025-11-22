a650d0cc29a9acf2b5e3e08bc34270c88238b0e5
RichBen03 <richmwendwa03@gmail.com>
2025-11-22 12:21:30 +0300

    used exoress validator to validate request from the client

---
+++ backend/middlewares/validateRequest.js
@@ -0,0 +1,20 @@
+import { validationResult } from 'express-validator';
+
+/**
+ * Middleware to validate request using express-validator
+ */
+const validateRequest = (req, res, next) => {
+  const errors = validationResult(req);
+  
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      success: false,
+      message: 'Validation failed',
+      errors: errors.array()
+    });
+  }
+  
+  next();
+};
+
+export default validateRequest;
